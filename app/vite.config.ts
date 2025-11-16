80cb1eb0c74bebdcf9004ade09ce82903abea434
peaceiam4now-hash <peaceiam4now@gmail.com>
2025-11-16 21:19:53 +0000

    chore: fix GH Pages workflow config

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,8 +1,8 @@
+// app/vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANT: this must match the repo name
-  base: '/toolkit/', // if the repo is peaceiam4now/toolkit
+  base: '/toolkit/', // repo name
 })
